5a994b78622cc5ef72d604481af4575309f5d2a1
zixuanliang <lzxlnac@gmail.com>
2022-07-16 03:09:42 +0900

    update for percolation

--- percolation/percolation/src/Percolation.java
+++ percolation/percolation/src/Percolation.java
@@ -1,75 +1,75 @@
-
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     // creates n-by-n grid, with all sites initially block, with value 0
-    public int[][][] grid;
-    public int n;
-    public int[] top;
-    public int[] bot;
-    public int num_open;
+    private int[][] grid;
+    private int n;
+    private int num_open;
+    private WeightedQuickUnionUF uf;
+    private int index_of_top;
+    private int index_of_bot;
 
     public Percolation(int n){
-        grid = new int[n][n][3];
-        for (int i = 1; i < n; i++){
-            for (int j = 1; j < n; j++){
-                grid[i][j][0] = i;
-                grid[i][j][1] = j;
-            }
-        }
-        top = new int[]{n + 1, n + 1, 1};
-        bot = new int[]{0, 0, 1};
+        grid = new int[n][n];
         this.n = n;
+        uf = new WeightedQuickUnionUF(n*n + 2);
+        index_of_top = n*n;
+        index_of_bot = index_of_top + 1;
     }
-
+    public int index(int row, int col) {
+        return (row - 1) * n + col - 1;
+    }
     // opens the site (row, col) if it is not open already, site with value 0 is blocked, 1 means open
     public void open(int row, int col){
-        int[] SITE = site(row, col);
-        if (SITE[3] == 0) {
-            SITE[3] = 1;
-            //change root
-            change_root(row, col, SITE);
+        if (row < 1 || row > n || col < 1 || col > n) {
+            throw new IllegalArgumentException();
+        }
+        if (grid[row][col] == 0) {
+            grid[row][col] = 1;
+            connect(row, col);
             num_open++;
         }
     }
-    public void change_root(int row, int col, int[] l){
-        if (find_nearest(row, col) != null) {
-            int[] from = find_nearest(row, col);
-            l[0] = from[0];
-            l[1] = from[1];
+    //connect the site with its possible neighbors
+    public void connect(int row, int col){
+        int i = index(row, col);
+        if (row == 1) {
+            uf.union(i, index_of_top);
+        } else {
+            connect_to(i, row - 1, col);
+        }
+        if (row == n) {
+            uf.union(i, index_of_bot);
+        } else {
+            connect_to(i, row + 1, col);
+        }
+        if (col < n) {
+            connect_to(i, row, col + 1);
+        }
+        if (col > 1) {
+            connect_to(i, row, col - 1);
         }
     }
-
-    public int[] find_nearest(int row, int col){
-        if (row == 1) {
-            return top;
-        } else if (row == n) {
-            return bot;
+    //connect a site with a specific site
+    public void connect_to(int i, int row, int col) {
+        if (isOpen(row, col)) {
+            uf.union(i, index(row, col));
         }
-        if (isOpen(row - 1, col)) {
-            return site(row - 1, col);
-        } else if (isOpen(row, col - 1)) {
-            return site(row, col - 1);
-        } else if (isOpen(row, col + 1)) {
-            return site(row, col + 1);
-        } else if (isOpen(row + 1, col) {
-            return site(row + 1, col);
-        }
-        return null;
     }
     // is the site (row, col) open?
     public boolean isOpen(int row, int col){
-        if (row > n || col > n || row < 1 || col < 1) {
-            throw new IllegalArgumentException("");
+        if (row < 1 || row > n || col < 1 || col > n) {
+            throw new IllegalArgumentException();
         }
-        return grid[row][col][3] == 1;
+        return grid[row - 1][col - 1] == 1;
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col){
-        return grid[row][col][3] == 0;
+        if (row < 1 || row > n || col < 1 || col > n) {
+            throw new IllegalArgumentException();
+        }
+        return grid[row - 1][col - 1] == 0;
     }
 
     // returns the number of open sites
@@ -79,21 +79,9 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates(){
-        return connected(top, bot);
+        return uf.connected(index_of_top, index_of_bot);
     }
-    //if two site are connected
-    public boolean connected(int[] a, int[] b){
-        if (a[0] != b[0]) {
-            return false;
-        } else if (a[1] != b[1]) {
-            return false;
-        }
-        return true;
-    }
-    // return the site
-    public int[] site(int col, int row){
-        return grid[row - 1][col - 1];
-    }
+
     // test client (optional)
     public static void main(String[] args){
 
